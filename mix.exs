74a4de5e868a061723bc7985c02981def7fe2b3f
Boris Kotov <bk@webdeb.de>
2018-02-10 01:02:55 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FileCache.MixProject do
   def project do
     [
       app: :file_cache,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
